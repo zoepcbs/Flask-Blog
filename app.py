34ff1915e0c0d3d0eba1e5bf186aba44d8a1f658
Zoe P. <112001279+zoepcbs@users.noreply.github.com>
2025-11-07 19:29:42 -0600

    Begin challenge

---
+++ app.py
@@ -0,0 +1,33 @@
+import sqlite3
+from flask import Flask, render_template, request, url_for, flash, redirect, abort
+
+# make a Flask application object called app
+app = Flask(__name__)
+app.config["DEBUG"] = True
+
+
+
+# Function to open a connection to the database.db file
+def get_db_connection():
+    # create connection to the database
+    conn = sqlite3.connect('database.db')
+    
+    # allows us to have name-based access to columns
+    # the database connection will return rows we can access like regular Python dictionaries
+    conn.row_factory = sqlite3.Row
+
+    #return the connection object
+    return conn
+
+
+# use the app.route() decorator to create a Flask view function called index()
+@app.route('/')
+def index():
+    
+    return "<h1>I want to die</h1>"
+
+
+# route to create a post
+
+
+app.run(port=5000)
