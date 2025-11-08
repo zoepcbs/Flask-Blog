dfaeebe56924e85445f4c1dec4c44832d742247c
Zoe P. <112001279+zoepcbs@users.noreply.github.com>
2025-11-07 23:36:56 -0600

    delete function pt. 1

--- app.py
+++ app.py
@@ -113,6 +113,32 @@ def edit(id):
         
     # if GET then display page
     return render_template('edit.html', post=post)
+
+# create a route to delete a post
+# delete page wil only be processed with a POST method
+# the post id is the url parameter
+@app.route('/<int:id/delete', methods=('POST',))
+
+def delete(id):
+
+    # get the post
+    post = get_post(id)
+
+    # connect to the database
+    conn = get_db_connection()
+
+    # execute a delete query
+    conn.execute('DELETE from posts WHERE id = ?', (id,))
+
+    # commit and close the connection
+    conn.commit()
+    conn.close()
+
+    # flash a success message
+    flash('"{}" was successfully deleted!'.format(post['title']))
+
+    # redirect to the homepage
+    return redirect(url_for('index'))
     
 
 app.run(port=5000)
