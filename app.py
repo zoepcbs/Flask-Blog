d5c052b5d5783728efe8cc8c35b4f64e537dd0ea
Zoe P. <112001279+zoepcbs@users.noreply.github.com>
2025-11-07 23:26:26 -0600

    made create and update functions

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template, request, url_for, flash, redirect, abo
 # make a Flask application object called app
 app = Flask(__name__)
 app.config["DEBUG"] = True
+app.config["SECRET_KEY"] = 'your secret key'
 
 
 
@@ -19,15 +20,99 @@ def get_db_connection():
     #return the connection object
     return conn
 
+# function to retrieve a post from the database
+def get_post(post_id):
+    conn = get_db_connection()
+    post = conn.execute('SELECT * FROM posts WHERE id = ?', (post_id,)).fetchone()
+    conn.close()
+
+    if post is None:
+        abort(404)
+    
+    return post
+
 
 # use the app.route() decorator to create a Flask view function called index()
 @app.route('/')
 def index():
-    
-    return "<h1>I want to die</h1>"
+
+    # get a connection to the database
+    conn = get_db_connection()
+
+    # execute a query to read all the posts from the posts table in the database
+    posts = conn.execute('SELECT * FROM posts').fetchall()
+
+    # close the connection
+    conn.close()
+
+    # send the posts to the index.html template to be displayed
+    return render_template('index.html', posts=posts)
 
 
 # route to create a post
+@app.route('/create/', methods=('GET', 'POST'))
 
+def create():
+
+    # determine if the page is being requested with a POST or GET request
+    if request.method == 'POST':
+
+        # get the title and content that was submitted
+        title = request.form['title']
+        content = request.form['content']
+
+        # display an error if title or content is not submitted
+
+
+        # else make a database connection and insert the blog post content
+        if not title:
+            flash("Title is required")
+        elif not content:
+            flash("Content is required")
+        else:
+            conn = get_db_connection()
+
+            # insert data into database
+            conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)', (title, content))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
+
+    return render_template('create.html')
+
+# create a route to edit a post. load page with get or post method
+# pass the post id as url parameter
+@app.route('/<int:id>/edit/', methods=('GET', 'POST'))
+
+def edit(id):
+
+    # get the post from the database with a select query for the post with that id
+    post = get_post(id)
+    
+    # determine if the page was request with GET or POST
+    # if POST, process the form data. get the data and validate it. update the post and redirect to the homepage
+    if request.method == 'POST':
+
+        # get the title and content
+        title = request.form['title']
+        content = request.form['content']
+
+        # if not title or content, flash an error message
+        if not title:
+            flash('Title is required')
+        elif not content:
+            flash('Content is required')
+        else:
+            conn = get_db_connection()
+            conn.execute('UPDATE posts SET title = ?, content = ? WHERE id = ?', (title, content, id))
+            conn.commit()
+            conn.close()
+
+            # redirect to the home page
+            return redirect(url_for('index'))
+        
+    # if GET then display page
+    return render_template('edit.html', post=post)
+    
 
 app.run(port=5000)
